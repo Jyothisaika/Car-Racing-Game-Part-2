d2315dcd1f0e995e213b429baa113e4578016139
Jyothisaika <96777274+Jyothisaika@users.noreply.github.com>
2022-05-24 17:47:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,143 +1,69 @@
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
+// Create a variable canvas
+var canvas = new fabric.Canvas('myCanvas');
+ 
 
-car1_width = 120;
-car1_height = 70;
-car1_image = "car1.png";
-car1_x = 10;
-car1_y = 10;
+block_y=1;
+block_x=1;
 
-car2_width = 120;
-car2_height = 70;
-car2_image = "car2.png";
-car2_x = 10;
-car2_y = 100;
+block_image_width = 350;
+block_image_height = 480;
 
-background_image = "racing.jpg";
+var block_image_object= "";
 
-function add() {
-	background_imgTag = new Image(); //defining a variable with a new image
-	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
-	background_imgTag.src = background_image;   // load image
+// Complete the function new_image() to add new image
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function(IMG) {
+        block_image_object = IMG;
+		block_image_object.scaleToWidth(block_image_width);
+		block_image_object.scaleToHeight(block_image_height);
+		block_image_object.set({top:block_y,left:block_x});
+		canvas.add(block_image_object);	
+        })
 
-	car1_imgTag = new Image(); //defining a variable with a new image
-	car1_imgTag.onload = uploadcar1; // setting a function, onloading this variable
-	car1_imgTag.src = car1_image;   // load image
-
-	car2_imgTag = new Image(); //defining a variable with a new image
-	car2_imgTag.onload = uploadcar2; // setting a function, onloading this variable
-	car2_imgTag.src = car2_image;   // load image
 }
-
-function uploadBackground() {
-	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
-}
-
-function uploadcar1() {
-	ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
-}
-function uploadcar2() {
-	ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
-}
-
 
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
 {
-	keyPressed = e.keyCode;
-	console.log(keyPressed);
-		if(keyPressed == '38')
-		{
-			car1_up();
-			console.log("up arrow key");
-		}
-		if(keyPressed == '40')
-		{
-			car1_down();
-			console.log("down arrow key");
-		}
-		if(keyPressed == '37')
-		{
-			car1_left();
-			console.log("left arrow key");
-		}
-		if(keyPressed == '39')
-		{
-			car1_right();
-			console.log("right arrow key");
-		}
-		if(keyPressed == '87')
-		{
-			car2_up();
-			console.log("key w");
-		}
-		if(keyPressed == '83')
-		{
-			car2_down();
-			console.log("key s");
-		}
-		if(keyPressed == '65')
-		{
-			car2_left();
-			console.log("key a");
-		}
-		if(keyPressed == '68')
-		{
-			car2_right();
-			console.log("key d");
-		}
-}
-function car1_up ()
-{
-	if(car1_y >=0)
+keyPressed = e.keyCode;
+console.log(keyPressed);
+	// Use appropriate keycode to add red image
+	if(keyPressed == '82' ) 
 	{
-		car1_y = car1_y-10;
-		console.log("when up arrow is pressed, x = "+ car1_x + " | y = "+car1_y);
-		uploadBackground();
-		uploadcar1();
-		uploadcar2();
+        block_x = 10;
+        new_image('red.jpg');
+        console.log('R');
 	}
+	// Use appropriate keycode to add green image
+	if(keyPressed =='71')
+	{
+		block_x = 310;
+        new_image('green.png');
+        console.log('G');
+	}
+	// Use appropriate keycode to add yellow image
+	if(keyPressed =='89' )
+	{
+		block_x = 560;
+        new_image('yellow.png');
+        console.log('Y');
+	}
+	// Use appropriate keycode to add pink image
+	if(keyPressed =='80')
+	{
+		block_x = 880;
+        new_image('pink.png');
+        console.log('P');
+	}
+	// Use appropriate keycode to add blue image
+	if(keyPressed =='66')
+	{
+		block_x = 1040;
+        new_image('blue.png');
+        console.log('B');
+	}
+	
 }
 
-function car1_down ()
-{
-	if(car1_y >=0)
-	{
-		car1_y = car1_y-10;
-		console.log("when down arrow is pressed, x = "+ car1_x + " | y = "+car1_y);
-		uploadBackground();
-		uploadcar1();
-		uploadcar2();
-	}
-}
-
-function car1_left ()
-{
-	if(car1_y >=0)
-	{
-		car1_y = car1_y-10;
-		console.log("when left arrow is pressed, x = "+ car1_x + " | y = "+car1_y);
-		uploadBackground();
-		uploadcar1();
-		uploadcar2();
-	}
-}
-
-function car1_right ()
-{
-	if(car1_y >=0)
-	{
-		car1_y = car1_y-10;
-		console.log("when right arrow is pressed, x = "+ car1_x + " | y = "+car1_y);
-		uploadBackground();
-		uploadcar1();
-		uploadcar2();
-	}
-}
-
-if(car1_x > 700)
-{
-	console.log("car1 Won")
-	document.getElementById('game_status') . innerHTML = " Car1 Won!!"
-}
